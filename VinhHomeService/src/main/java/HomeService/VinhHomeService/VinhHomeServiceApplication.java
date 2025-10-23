3d4874004006f454ce97793c6a00165c93a6606d
jinnguyen09 <thiepthanhx09@gmail.com>
2025-10-23 23:10:14 +0700

    app

---
+++ VinhHomeService/src/main/java/HomeService/VinhHomeService/VinhHomeServiceApplication.java
@@ -0,0 +1,13 @@
+package HomeService.VinhHomeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinhHomeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinhHomeServiceApplication.class, args);
+	}
+
+}
---
+++ VinhHomeService/src/test/java/HomeService/VinhHomeService/VinhHomeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeService.VinhHomeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinhHomeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
